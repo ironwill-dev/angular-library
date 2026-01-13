9598eb19639fd6b7871987d2181136753e14c64a
ironwill-dev <cortezwilliams2002@gmail.com>
2026-01-12 21:02:33 -0800

    routing skeleton complete

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,31 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { BooksComponent } from './books/books.component';
+import { LoansComponent } from './loans/loans.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'login',
+        component: LoginComponent
+    },
+    {
+        path: 'dashboard',
+        component: DashboardComponent,
+        children: [
+            {
+                path: 'books',
+                component: BooksComponent
+            },
+            {
+                path: 'loans',
+                component: LoansComponent
+            }
+        ]
+    },
+    {
+        path: '',
+        redirectTo: 'login',
+        pathMatch: 'full'
+    }   
+];
---
+++ src/app/dashboard/dashboard.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { Router } from 'express';
+
+@Component({
+  selector: 'app-dashboard',
+  imports: [RouterModule],
+  templateUrl: './dashboard.component.html',
+  styleUrl: './dashboard.component.css'
+})
+export class DashboardComponent {
+
+}
