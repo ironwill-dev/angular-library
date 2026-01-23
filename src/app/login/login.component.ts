f146c799837560683ca25ce209903429d3671cb3
ironwill-dev <cortezwilliams2002@gmail.com>
2026-01-22 21:18:26 -0800

    Login Mock Auth Complete

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,22 +1,37 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthService } from '../auth/auth.service';
+import { ReactiveFormsModule, FormBuilder, Validators, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+  loginForm!: FormGroup; 
+
   constructor(
+    private fb: FormBuilder,
     private authService: AuthService, 
     private router: Router
   ) {}
 
   login() {
+    if (this.loginForm.invalid) {
+      return;
+    }
+
     this.authService.login()
     this.router.navigate(['/dashboard/books']);
+  }
+
+  ngOnInit() {
+    this.loginForm = this.fb.group({
+      username: ['', Validators.required],
+      password: ['', Validators.required]
+    });
   }
 }
